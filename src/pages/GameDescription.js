127c2c64dcb95119f60978ad22144c71c735eac0
VividTruthKeeper <mr.aydogdy02@gmail.com>
2022-01-20 14:06:52 +0500

    Added background in GameDescription

--- src/pages/GameDescription.js
+++ src/pages/GameDescription.js
@@ -1,8 +1,23 @@
 // IMPORT MODULES
 import React from "react";
 
+// IMPORT COMPONENTS
+import Breadcrumb from "../components/Breadcrumb";
+
 const GameDescription = () => {
-  return <h1>Game Description</h1>;
+  return (
+    <section className="game-desc">
+      <div className="bg"></div>
+      <div className="container">
+        <div className="game-desc-inner">
+          <Breadcrumb
+            linkTitles={["the last of us part ||"]}
+            linkTargets={["/game"]}
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default GameDescription;

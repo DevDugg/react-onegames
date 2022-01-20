81444b224cfccf4c7068882258e69d39c72d9677
VividTruthKeeper <mr.aydogdy02@gmail.com>
2022-01-20 18:11:39 +0500

    GameDescription half finished

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Routes, Route } from "react-router-dom";
 import "./styles/style.scss";
 
 // IMPORT COMPONENTS
-import Nav from './components/Nav';
+import Nav from "./components/Nav";
 
 // IMPORT PAGES
 import Home from "./pages/Home";
@@ -16,7 +16,6 @@ const App = () => {
   return (
     <div className="App">
       <Nav />
-      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/game" element={<GameDescription />} />

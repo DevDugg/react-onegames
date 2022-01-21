ae4a6f36eb3c557a2d5b099f616c289bf21e5587
VividTruthKeeper <mr.aydogdy02@gmail.com>
2022-01-21 23:41:54 +0500

    Div to Link

--- src/components/GridItem.js
+++ src/components/GridItem.js
@@ -1,17 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const GridItem = ({img, title, rating}) => {
-    return(
-        <div className="grid-item">
-            <div className="grid-item-img">
-                <img src={img} alt={title} />
-            </div>
-            <div className="grid-item-desc">
-                <h3 className="grid-item-title">{title}</h3>
-                <div className="grid-item-score">{rating}</div>
-            </div>
+const GridItem = ({ img, title, rating }) => {
+  return (
+    <Link to="/game">
+      <div className="grid-item">
+        <div className="grid-item-img">
+          <img src={img} alt={title} />
         </div>
-    );
-}
+        <div className="grid-item-desc">
+          <h3 className="grid-item-title">{title}</h3>
+          <div className="grid-item-score">{rating}</div>
+        </div>
+      </div>
+    </Link>
+  );
+};
 
 export default GridItem;

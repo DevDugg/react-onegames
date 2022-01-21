78b916dd371ffc423f15d4e393dd29472bcf9c89
VividTruthKeeper <mr.aydogdy02@gmail.com>
2022-01-22 00:00:36 +0500

    Styling corrections

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,20 +4,22 @@ import { Link } from "react-router-dom";
 import search from "../images/search.svg";
 
 const Nav = () => {
-    return (
-        <nav className="navbar">
-            <div className="container">
-                <Link to="/" className="logo">OneGames</Link>
-                <div className="nav-menu">
-                    <div className="search">
-                        <img src={search} alt="search" />
-                        <input type="text" placeholder="Search" />
-                    </div>
-                    <Link className="nav-link" to="/about">ABOUT</Link>
-                </div>
-            </div>
-        </nav>
-    );
-}
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <Link to="/" className="logo">
+          OneGames
+        </Link>
+        <div className="search">
+          <img src={search} alt="search" />
+          <input type="text" placeholder="Search" />
+        </div>
+        <Link className="nav-link" to="/about">
+          ABOUT
+        </Link>
+      </div>
+    </nav>
+  );
+};
 
 export default Nav;

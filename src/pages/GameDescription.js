2ce7627e66536339dbd99e988f28aabaa6571dd5
VividTruthKeeper <mr.aydogdy02@gmail.com>
2022-01-24 00:26:11 +0500

    Restructured for better mobile v

--- src/pages/GameDescription.js
+++ src/pages/GameDescription.js
@@ -51,75 +51,72 @@ const GameDescription = () => {
       <div className="container">
         <div className="game-desc-inner">
           <div className="game-info">
-            <div className="game-info__left">
-              <div className="game-info__left__top">
-                <Breadcrumb
-                  linkTitles={["the last of us part ii"]}
-                  linkTargets={["/game"]}
-                />
-                <div className="game-info__left-meta">
-                  <h3 className="release">june 19, 2020</h3>
-                  <h4 className="score">95</h4>
-                </div>
-                <h1 className="game-name">The last of us part ii</h1>
+            <div className="game-info__left__top">
+              <Breadcrumb
+                linkTitles={["the last of us part ii"]}
+                linkTargets={["/game"]}
+              />
+              <div className="game-info__left-meta">
+                <h3 className="release">june 19, 2020</h3>
+                <h4 className="score">95</h4>
               </div>
-              <div className="game-info__left__middle">
-                <h3>About</h3>
-                <h5 className="game-about">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
-                  aliquam, purus sit amet luctus venenatis, lectus magna
-                  fringilla urna, porttitor rhoncus dolor purus non enim
-                  praesent elementum facilisis leo, vel fringilla est
-                  ullamcorper eget nulla facilisi etiam dignissim diam quis enim
-                  lobortis scelerisque fermentum dui faucibus in ornare quam
-                  viverrah
-                </h5>
+              <h1 className="game-name">The last of us part ii</h1>
+            </div>
+            <div className="game-info__left__middle">
+              <h3>About</h3>
+              <h5 className="game-about">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
+                aliquam, purus sit amet luctus venenatis, lectus magna fringilla
+                urna, porttitor rhoncus dolor purus non enim praesent elementum
+                facilisis leo, vel fringilla est ullamcorper eget nulla facilisi
+                etiam dignissim diam quis enim lobortis scelerisque fermentum
+                dui faucibus in ornare quam viverrah
+              </h5>
+            </div>
+            <div className="game-info__left__bottom">
+              <div className="game-char-block plat-block">
+                <h6>Platforms</h6>
+                <ul>
+                  <li>
+                    <h3>PlayStation 4</h3>
+                  </li>
+                  <li>
+                    <h3>PlayStation 5</h3>
+                  </li>
+                </ul>
               </div>
-              <div className="game-info__left__bottom">
-                <div className="game-char-block plat-block">
-                  <h6>Platforms</h6>
-                  <ul>
-                    <li>
-                      <h3>PlayStation 4</h3>
-                    </li>
-                    <li>
-                      <h3>PlayStation 5</h3>
-                    </li>
-                  </ul>
-                </div>
-                <div className="game-char-block release-block">
-                  <h6>Release Date</h6>
-                  <ul>
-                    <li>
-                      <h3>june 19, 2020</h3>
-                    </li>
-                  </ul>
-                </div>
-                <div className="game-char-block score-block">
-                  <h6>Rating</h6>
-                  <ul>
-                    <li>
-                      <h4 className="score">95</h4>
-                    </li>
-                  </ul>
-                </div>
-                <div className="game-char-block genre-block">
-                  <h6>Genre</h6>
-                  <ul>
-                    <li>
-                      <h3>Action</h3>
-                    </li>
-                    <li>
-                      <h3>Shooter</h3>
-                    </li>
-                    <li>
-                      <h3>Adventure</h3>
-                    </li>
-                  </ul>
-                </div>
+              <div className="game-char-block release-block">
+                <h6>Release Date</h6>
+                <ul>
+                  <li>
+                    <h3>june 19, 2020</h3>
+                  </li>
+                </ul>
+              </div>
+              <div className="game-char-block score-block">
+                <h6>Rating</h6>
+                <ul>
+                  <li>
+                    <h4 className="score">95</h4>
+                  </li>
+                </ul>
+              </div>
+              <div className="game-char-block genre-block">
+                <h6>Genre</h6>
+                <ul>
+                  <li>
+                    <h3>Action</h3>
+                  </li>
+                  <li>
+                    <h3>Shooter</h3>
+                  </li>
+                  <li>
+                    <h3>Adventure</h3>
+                  </li>
+                </ul>
               </div>
             </div>
-            <div className="game-info__right">
+            <div className="game-images">
               <div className="game-video">
                 <img src={Video} alt="game" />
               </div>

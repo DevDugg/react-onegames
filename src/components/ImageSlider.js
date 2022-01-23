9446cba976cd195c627228b081e406657c4d3b71
VividTruthKeeper <mr.aydogdy02@gmail.com>
2022-01-23 13:32:24 +0500

    Bottom slider props fixed

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -2,6 +2,10 @@
 import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { FreeMode, Navigation, Thumbs } from "swiper";
+
+// IMPORT CUSTOM HOOKS
+import useMediaQuery from "../hooks/useMediaQuery";
+
 // IMPORT SWIPER STYLES
 import "swiper/css";
 import "swiper/css/navigation";
@@ -11,7 +15,21 @@ import "swiper/css/pagination";
 SwiperCore.use([FreeMode, Navigation, Thumbs]);
 
 const ImageSlider = ({ images, activeImageId, setActiveImageId }) => {
+  // SWIPER STATE
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  // ====================
+
+  // SHIT CODE
+  const m1530 = useMediaQuery("(min-width: 1530px)");
+  const m1410 = useMediaQuery("(min-width: 1410px)");
+  const m1270 = useMediaQuery("(min-width: 1270px)");
+  const m1130 = useMediaQuery("(min-width: 1130px)");
+  const m990 = useMediaQuery("(min-width: 990px)");
+  const m850 = useMediaQuery("(min-width: 850px)");
+  const m710 = useMediaQuery("(min-width: 710px)");
+  const m590 = useMediaQuery("(min-width: 590px)");
+  const m430 = useMediaQuery("(min-width: 430px)");
+  // ===================
   return (
     <section className="slider">
       <div className="slider-container">
@@ -39,28 +57,46 @@ const ImageSlider = ({ images, activeImageId, setActiveImageId }) => {
             </Swiper>
           </div>
           <div className="slider__bottom">
-            {/* <div className="slider__bottom-inner"> */}
             <Swiper
               onSwiper={setThumbsSwiper}
-              spaceBetween={10}
-              slidesPerView={12}
+              spaceBetween={16}
+              slidesPerView={
+                // ANOTHER SHIT CODE
+                m1530
+                  ? 10
+                  : m1410
+                  ? 9
+                  : m1270
+                  ? 8
+                  : m1130
+                  ? 7
+                  : m990
+                  ? 6
+                  : m850
+                  ? 5
+                  : m710
+                  ? 4
+                  : m590
+                  ? 3
+                  : m430
+                  ? 2
+                  : 3
+                // ================
+              }
               freeMode={true}
               watchSlidesProgress={true}
               className="mySwiper"
             >
               {images.map((image, i) => {
                 return (
-                  <SwiperSlide
-                    data-active={i}
-                    key={i}
-                    className="slider-img-bottom"
-                  >
-                    <img src={image} alt={image} />
+                  <SwiperSlide data-active={i} key={i}>
+                    <div className="slider-img-bottom">
+                      <img src={image} alt={image} />
+                    </div>
                   </SwiperSlide>
                 );
               })}
             </Swiper>
-            {/* </div> */}
           </div>
         </div>
       </div>

3c1d7d56192d91b6196fe347f2240b3acc58c972
VividTruthKeeper <mr.aydogdy02@gmail.com>
2022-02-01 00:09:02 +0500

    loaders on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,20 @@
 // IMPORT MODULES
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 // Importing components
 import Header from "../components/Header";
 import Grid from "../components/Grid";
-import axios from "axios";
+
+import Loader from "../components/Loader";
 
 // AUTH
 import { auth } from "../auth";
 
 const Home = () => {
   // API
+  const [pageSize, setPageSize] = useState(20);
+  const [loading, setLoading] = useState(false);
   const [apiData1, setApiData1] = useState();
   const [apiData2, setApiData2] = useState();
   const [apiData3, setApiData3] = useState();
@@ -48,42 +53,44 @@ const Home = () => {
   const authKey = auth;
 
   // Popular games
-  const popular_games = `games?key=${authKey}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=40`;
+  const popular_games = `games?key=${authKey}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=${pageSize}`;
 
   // Popular games URL
   const popularGamesURL = `${base_url}${popular_games}`;
 
+  const fetchData = (link1, link2, link3, link4) => {
+    setLoading(true);
+    axios
+      .get(link1)
+      .then((data) => {
+        setLoading(false);
+        setApiData1(data.data.results);
+      })
+      .catch((err) => console.log(err));
+
+    axios
+      .get(link2)
+      .then((data) => {
+        setApiData2(data.data.results);
+      })
+      .catch((err) => console.log(err));
+
+    axios
+      .get(link3)
+      .then((data) => {
+        setApiData3(data.data.results);
+      })
+      .catch((err) => console.log(err));
+
+    axios
+      .get(link4)
+      .then((data) => {
+        setApiData4(data.data.results);
+      })
+      .catch((err) => console.log(err));
+  };
+
   useEffect(() => {
-    const fetchData = (link1, link2, link3, link4) => {
-      axios
-        .get(link1)
-        .then((data) => {
-          setApiData1(data.data.results);
-        })
-        .catch((err) => console.log(err));
-
-      axios
-        .get(link2)
-        .then((data) => {
-          setApiData2(data.data.results);
-        })
-        .catch((err) => console.log(err));
-
-      axios
-        .get(link3)
-        .then((data) => {
-          setApiData3(data.data.results);
-        })
-        .catch((err) => console.log(err));
-
-      axios
-        .get(link4)
-        .then((data) => {
-          setApiData4(data.data.results);
-        })
-        .catch((err) => console.log(err));
-    };
-
     fetchData(
       popularGamesURL + "&page=1",
       popularGamesURL + "&page=2",
@@ -103,7 +110,28 @@ const Home = () => {
   return (
     <div className="container">
       <Header setGrid={setGrid} grid={grid} />
-      <Grid grid={grid} data={[apiData1, apiData2, apiData3, apiData4]} />
+      {!apiData1 ? (
+        <Loader width={"20rem"} height={"20rem"} />
+      ) : (
+        <Grid grid={grid} data={[apiData1, apiData2, apiData3, apiData4]} />
+      )}
+
+      <div className="more">
+        <button
+          type="button"
+          onClick={() => {
+            setPageSize(pageSize + 20);
+            fetchData(
+              popularGamesURL + "&page=1",
+              popularGamesURL + "&page=2",
+              popularGamesURL + "&page=3",
+              popularGamesURL + "&page=4"
+            );
+          }}
+        >
+          {loading ? <Loader width={"5rem"} height={"5rem"} /> : "More"}
+        </button>
+      </div>
     </div>
   );
 };
